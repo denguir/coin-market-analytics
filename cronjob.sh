e11c24454935b188cf15bfb2d44ecfcb69e8028c
denguir <anass.den@gmail.com>
2021-05-12 17:00:15 +0000

    database update

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,6 @@
 
 python3 ~/home/coin-market-analytics/main.py
+git -C ~/home/coin-market-analytics pull origin master
 git -C ~/home/coin-market-analytics add .
 git -C ~/home/coin-market-analytics commit -a -m "database update"
 git -C ~/home/coin-market-analytics push origin master
